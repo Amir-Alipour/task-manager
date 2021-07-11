8d88f5a007922e7fd62768e734f38640ce4ec571
Amir Alipour <amir.alipour12127@gmail.com>
2021-07-11 20:01:53 +0430

    modal add member created

--- src/components/addMember/AddMember.jsx
+++ src/components/addMember/AddMember.jsx
@@ -1,9 +1,53 @@
-function AddMember() {
+function AddMember({ func }) {
     return (
-        <div className="z-50 w-full h-screen absolute top-0 bg-black bg-opacity-40">
-            
+        <div className="z-50 h-screen absolute top-0 bg-black bg-opacity-50 -left-2">
+            <div className="container py-10 flex items-center justify-center">
+                <div
+                    className="bg-gray-100 rounded shadow box-border p-4 mt-5"
+                    style={{ height: "50vh", width: "60%" }}
+                >
+                    <div className="row mt-5">
+                        <h3>Add Member 👨‍💻</h3>
+                    </div>
+                    <div className="row mt-3">
+                        <div className="col-12 mt-2">
+                            <label htmlFor="username">Username</label>
+                            <input
+                                placeholder="David"
+                                id="username"
+                                type="text"
+                                className="form-control shadow-none mt-2"
+                            />
+                        </div>
+                        <div className="col-12 mt-4">
+                            <label htmlFor="profile">Profile</label>
+                            <input
+                                placeholder="URL"
+                                id="profile"
+                                type="text"
+                                className="form-control shadow-none mt-2"
+                            />
+                        </div>
+                    </div>
+                    <div className="row mt-5 float-left">
+                        <div className="col-6">
+                            <button className="p-2 px-4 bg-blue-500 text-white rounded hover:bg-blue-400">
+                                Add
+                            </button>
+                        </div>
+                        <div className="col-6">
+                            <button
+                                className="p-2 px-4 -ml-3 border-1 border-red-500 text-red-500 rounded"
+                                onClick={() => func()}
+                            >
+                                cancle
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-    )
+    );
 }
 
-export default AddMember
+export default AddMember;
--- src/pages/worksapce/WorkspaceHeader.jsx
+++ src/pages/worksapce/WorkspaceHeader.jsx
@@ -1,11 +1,14 @@
+import { useState } from "react";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import AddMember from "../../components/addMember/AddMember";
 import { fetchUsers } from "../../store/usersSlice/usersActions";
 import TeamProfile from "./TeamProfile";
 
 function WorkspaceHeader() {
     const dispatch = useDispatch();
     const status = useSelector((state) => state.users.status);
+    const [isModalOpen, setIsModalOpen] = useState(true);
 
     useEffect(() => {
         if (status === "idle") {
@@ -13,35 +16,46 @@ function WorkspaceHeader() {
         }
     }, [dispatch, status]);
 
+    const handleOpenAddMemberModal = () => {
+        setIsModalOpen((prev) => !prev);
+        isModalOpen
+            ? (document.querySelector("body").style.overflowY = "scroll")
+            : (document.querySelector("body").style.overflowY = "hidden");
+    };
+
     return (
         <>
-            {status === "loading" ? (
-                null
-            ) : (
-                <div className="col-12 h-full flex items-center justify-between">
-                    <div className="flex items-center">
-                        <div className="hidden md:flex">
-                            <h3>Team &nbsp; | &nbsp;</h3>
+            {status === "loading" ? null : (
+                <>
+                    {isModalOpen ? <AddMember func={handleOpenAddMemberModal} /> : null}
+                    <div className="col-12 h-full flex items-center justify-between">
+                        <div className="flex items-center">
+                            <div className="hidden md:flex">
+                                <h3>Team &nbsp; | &nbsp;</h3>
+                            </div>
+                            <TeamProfile />
+                            <div className="hidden md:flex ml-4">
+                                <button
+                                    className="flex"
+                                    onClick={handleOpenAddMemberModal}
+                                >
+                                    <p className="text-blue-300 mt-4">
+                                        add member ..
+                                    </p>
+                                </button>
+                            </div>
                         </div>
-                        <TeamProfile />
-                        <div className="hidden md:flex ml-4">
-                            <button className="flex">
-                                <p className="text-blue-300 mt-4">
-                                    add member ..
-                                </p>
+
+                        <div>
+                            <button className="bg-blue-500 p-2 text-white rounded shadow hover:bg-blue-400">
+                                Add Task
+                            </button>
+                            <button className="border p-2 rounded ml-3 md:mx-3">
+                                history
                             </button>
                         </div>
                     </div>
-
-                    <div>
-                        <button className="bg-blue-500 p-2 text-white rounded shadow hover:bg-blue-400">
-                            Add Task
-                        </button>
-                        <button className="border p-2 rounded ml-3 md:mx-3">
-                            history
-                        </button>
-                    </div>
-                </div>
+                </>
             )}
         </>
     );
